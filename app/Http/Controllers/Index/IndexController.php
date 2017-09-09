21512a2372a40f987ed23ff853a11a270e87aa73
zhanganyuan <zhanganyuan@qiyiniu.cn>
2017-09-09 17:50:00 +0800

    add replay

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -27,4 +27,29 @@ class IndexController extends Controller
             $this->reponseMsg();
         }
     }
+
+    //判断消息类型并调用相应类型回复方法
+    private function reponseMsg()
+    {
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        /*<xml>
+         <ToUserName><![CDATA[toUser]]></ToUserName>
+         <FromUserName><![CDATA[fromUser]]></FromUserName>
+         <CreateTime>1348831860</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[this is a test]]></Content>
+         <MsgId>1234567890123456</MsgId>
+         </xml>    消息推送的xml数据结构*/
+
+        $postObj = simplexml_load_string($postArr);
+        //判断消息类型
+        if(strtolower($postObj->MsgType == 'event')){
+//            echo 'event';
+//            $this->eventSubscribe($postObj);
+        }else if(strtolower($postObj->MsgType) == 'text'){
+//            echo 'text';
+//            $this->textTypeMsg($postObj);
+        }
+
+    }
 }
